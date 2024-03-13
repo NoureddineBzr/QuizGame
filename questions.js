2c3f84c7f41cb5f588d5bd648221251ae4e6147e
Noureddine Bouzire <bouzire.noureddine1@gmail.com>
2024-03-13 14:59:50 +0000

    Update

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+      numb: 1,
+      question: "What is the primary purpose of a constructor in Java?",
+      answer: "To initialize objects",
+      options: [
+        "To execute code only once in a class",
+        "To perform mathematical calculations",
+        "To initialize objects",
+        "To define the superclass"
+      ],
+    },
+    {
+      numb: 2,
+      question: "What is the correct syntax to create an object in Java?",
+      answer: "ClassName objectName = new ClassName();",
+      options: [
+        "ClassName objectName = ClassName();",
+        "ClassName objectName = new objectName();",
+        "ClassName objectName = new ClassName();",
+        "ClassName objectName = ClassName;"
+      ],
+    },
+    {
+      numb: 3,
+      question: "Which keyword is used to prevent method overriding in Java?",
+      answer: "final",
+      options: [
+        "override",
+        "abstract",
+        "super",
+        "final"
+      ],
+    },
+    {
+      numb: 4,
+      question: "What does JVM stand for?",
+      answer: "Java Virtual Machine",
+      options: [
+        "Java Virtual Memory",
+        "Java Virtual Model",
+        "Java Verified Module",
+        "Java Virtual Machine"
+      ],
+    },
+    {
+      numb: 5,
+      question: "Which data type in Java is used to create a single-precision floating point number?",
+      answer: "float",
+      options: [
+        "int",
+        "double",
+        "float",
+        "String"
+      ],
+    },
+  ];
